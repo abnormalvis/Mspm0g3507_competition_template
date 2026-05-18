dc9254794376da12591fcca33f0e40c731b5e1ff
abnormalvis <2136555857@qq.com>
2026-05-18 23:09:35 +0800

    update: modified pwm config

--- MSPM0G3507_Project_template/ti_msp_dl_config.h
+++ MSPM0G3507_Project_template/ti_msp_dl_config.h
@@ -83,24 +83,12 @@ extern "C" {
 #define PWM_0_INST_IRQHandler                                   TIMA0_IRQHandler
 #define PWM_0_INST_INT_IRQN                                     (TIMA0_INT_IRQn)
 #define PWM_0_INST_CLK_FREQ                                             80000000
-/* GPIO defines for channel 0 */
-#define GPIO_PWM_0_C0_PORT                                                 GPIOA
-#define GPIO_PWM_0_C0_PIN                                         DL_GPIO_PIN_21
-#define GPIO_PWM_0_C0_IOMUX                                      (IOMUX_PINCM46)
-#define GPIO_PWM_0_C0_IOMUX_FUNC                     IOMUX_PINCM46_PF_TIMA0_CCP0
-#define GPIO_PWM_0_C0_IDX                                    DL_TIMER_CC_0_INDEX
 /* GPIO defines for channel 1 */
 #define GPIO_PWM_0_C1_PORT                                                 GPIOA
 #define GPIO_PWM_0_C1_PIN                                          DL_GPIO_PIN_7
 #define GPIO_PWM_0_C1_IOMUX                                      (IOMUX_PINCM14)
 #define GPIO_PWM_0_C1_IOMUX_FUNC                     IOMUX_PINCM14_PF_TIMA0_CCP1
 #define GPIO_PWM_0_C1_IDX                                    DL_TIMER_CC_1_INDEX
-/* GPIO defines for channel 2 */
-#define GPIO_PWM_0_C2_PORT                                                 GPIOA
-#define GPIO_PWM_0_C2_PIN                                          DL_GPIO_PIN_3
-#define GPIO_PWM_0_C2_IOMUX                                       (IOMUX_PINCM8)
-#define GPIO_PWM_0_C2_IOMUX_FUNC                      IOMUX_PINCM8_PF_TIMA0_CCP2
-#define GPIO_PWM_0_C2_IDX                                    DL_TIMER_CC_2_INDEX
 /* GPIO defines for channel 3 */
 #define GPIO_PWM_0_C3_PORT                                                 GPIOA
 #define GPIO_PWM_0_C3_PIN                                         DL_GPIO_PIN_23

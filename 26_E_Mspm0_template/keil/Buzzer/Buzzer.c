32c62e497fdf4f0a54d5ba7d63127dcb4c87f16e
abnormalvis <2136555857@qq.com>
2026-07-31 22:34:28 +0800

    feature: debug buzzer

--- 26_E_Mspm0_template/keil/Buzzer/Buzzer.c
+++ 26_E_Mspm0_template/keil/Buzzer/Buzzer.c
@@ -3,22 +3,29 @@
 bool beep_on_1s_flag;
 uint32_t Last_TimerCount;
 
-void Buzzer_on(){
-    DL_GPIO_setPins(Beep_PORT, Beep_buzzer_PIN);
-};
+void Buzzer_on(void){
+		DL_GPIO_setPins(Beep_PORT, Beep_buzzer_PIN);     /* active-low: HIGH = buzzer off */
+}
 
-void Buzzer_off(){
-    DL_GPIO_clearPins(Beep_PORT, Beep_buzzer_PIN);
-};
+void Buzzer_off(void){
+    DL_GPIO_clearPins(Beep_PORT, Beep_buzzer_PIN);   /* active-low: LOW = buzzer on */
+}
+
+void Buzzer_Init(void){
+    BUZZER_OFF();   /* ensure buzzer starts silent */
+}
 
 //放1ms中断，参数是TimerCount,通过置一beep_on_1s_flag使蜂鸣器响1s
 void Buzzer_on_1s(uint32_t TimerCount){
+    static uint8_t beep1s_done = 0;
     if(beep_on_1s_flag == 1){
         Last_TimerCount = TimerCount;
-        Buzzer_on();
+        beep1s_done = 0;
+        BUZZER_ON();
         beep_on_1s_flag = 0;
     }
-    if (TimerCount-Last_TimerCount >= 1000) {
-        Buzzer_off();
+    if (!beep1s_done && (TimerCount - Last_TimerCount >= 1000)) {
+        BUZZER_OFF();
+        beep1s_done = 1;
     }
-};
+}
--- 26_E_Mspm0_template/keil/Buzzer/Buzzer.h
+++ 26_E_Mspm0_template/keil/Buzzer/Buzzer.h
@@ -6,8 +6,20 @@
 
 extern bool beep_on_1s_flag;
 
-void Buzzer_on();
-void Buzzer_off();
+/* ---- Polarity: uncomment if your buzzer is active-HIGH ---- */
+// #define BUZZER_ACTIVE_HIGH
+
+#ifdef BUZZER_ACTIVE_HIGH
+    #define BUZZER_ON()  Buzzer_off()   /* HIGH = on  */
+    #define BUZZER_OFF() Buzzer_on()    /* LOW  = off */
+#else
+    #define BUZZER_ON()  Buzzer_on()    /* LOW  = on  (default active-low) */
+    #define BUZZER_OFF() Buzzer_off()   /* HIGH = off (default active-low) */
+#endif
+
+void Buzzer_Init(void);
+void Buzzer_on(void);
+void Buzzer_off(void);
 //放1ms中断,若beef_on_1s_flag=1,蜂鸣器响
 //注意若使用此函数则上面的函数无效
 void Buzzer_on_1s(uint32_t TimerCount);
